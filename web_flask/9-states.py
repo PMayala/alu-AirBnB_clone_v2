822ae0976ccd9e551276443b05297cb6cbfb0566
PMayala <p.mayala@alustudent.com>
2024-06-10 09:12:58 +0000

    Request curl 0.0.0.0:5000/states with 3 States in DB + add one new State (with 2 cities)

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,30 +1,57 @@
 #!/usr/bin/python3
-"""Start web application with two routings
 """
-
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
+from models.city import City
+import uuid
+
 app = Flask(__name__)
-
-
-@app.route('/states')
-@app.route('/states/<id>')
-def states_list(id=None):
-    """Render template with states
-    """
-    path = '9-states.html'
-    states = storage.all(State)
-    return render_template(path, states=states, id=id)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
+def teardown_db(exception):
+    """
+    After each request, this method calls storage.close()
     """
     storage.close()
 
 
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Displays an HTML page with a list of all states and their cities
+    """
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    Displays an HTML page with a list of all states and their cities
+    """
+    states = storage.all(State).values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
+def states(state_id=None):
+    """
+    Displays an HTML page with a list of all states and their cities,
+    or a specific state and its cities
+    """
+    states = storage.all(State).values()
+    state_obj = None
+    if state_id:
+        state_key = 'State.' + str(uuid.UUID(state_id))
+        state_obj = storage.all(State).get(state_key)
+    return render_template('9-states.html', states=states, state=state_obj)
+
+
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
